f5dbdb996363911ca908b9bd9b2e45584b9bc4d6
shashiweb <shashiweb64@gmail.com>
2025-01-12 12:57:37 +0530

    change

--- src/About.js
+++ src/About.js
@@ -1,9 +1,16 @@
+import { Fragment } from "react"
+const a = 50;
+let c = "red";
+
 function Aboutpage()
 {
     return(
-        <section>
-            this is my template
-        </section>
+        <Fragment>
+        <h1>this is first <h2>heading two</h2> page</h1>
+        <h2 className="xyz">sdfhsjdfhjsdf</h2>
+        <h3 style={{color:'blue',backgroundColor:'green',fontSize:a,border:`10px solid ${c}`}}>this is inline css in react js</h3>
+
+        </Fragment>
     )
 }
 export default Aboutpage
